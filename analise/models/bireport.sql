494865a787c337edc95f6cbe24b52addb9d33fc0
Romulo Vieira <romulo.vieira777@gmail.com>
2023-09-19 16:49:20 -0300

    dbt_udemy

--- analise/models/bireport.sql
+++ analise/models/bireport.sql
@@ -2,12 +2,12 @@
     config(
         materialized='table',
         pre_hook=["
-            begin; lock table {{ target.schema }}.bireport;
+            begin; lock table {{ target.schema }}.;
         "],
         post_hook=["
             COMMIT;
-            GRANT USAGE ON SCHEMA {{ target.schema }} TO GROUP biusers;
-            GRANT SELECT ON TABLE {{ target.schema }}.bireport TO GROUP biusers;
+            GRANT USAGE ON SCHEMA {{ target.schema }} TO GROUP ;
+            GRANT SELECT ON TABLE {{ target.schema }}. TO GROUP ;
         "]
     )
 }}
